178e7a35d173ae3f02cc2dae005bc1285ad32702
ChaiCaroline <chaiene_caroline@hotmail.com>
2023-09-12 16:37:15 -0300

    ajustes, clear code

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,101 +1,94 @@
+import { cardapio } from "./cardapio";
+
 class CaixaDaLanchonete {
-    cardapio = {
-        'cafe': 3.00,
-        'chantily': 1.50,
-        'suco': 6.20,
-        'sanduiche': 6.50,
-        'queijo': 2.00,
-        'salgado': 7.25,
-        'combo1': 9.50,
-        'combo2': 7.50
-    };
-
-    pagamento(metodo){
-        let pagamento = 0
-        if (metodo.includes('dinheiro')){
-            return pagamento = -0.05
-        }else if (metodo.includes('credito')) {
-            return pagamento = 0.03
-        }else if (metodo.includes('debito')){
-            return pagamento = 1
-        }else {
-            return false
-        }
-    }
-    pedido(itens){
-        let pedidoEQuantidades = [];
-        for (let index = 0; index < itens.length; index++) {
-            if(itens[index].includes('chantily')){
-                let temItemPrincipal = 0
-                for (let indext = 0; indext < itens.length; indext++) {
-                    if (itens[indext].includes('cafe')){
-                        temItemPrincipal ++
-                    }
-                }
-                if(temItemPrincipal === 0){
-                    return 3
-                }
-            }
-        }
-        for (let index = 0; index < itens.length; index++) {
-            if(itens[index].includes('queijo')){
-                let temItemPrincipal = 0
-                for (let indext = 0; indext < itens.length; indext++) {
-                    if (itens[indext].includes('sanduiche')){
-                        temItemPrincipal ++
-                    }
-                }
-                if(temItemPrincipal === 0){
-                    return 3
-                }
-            }
-        }
-        itens.forEach(element => {
-            pedidoEQuantidades.push(element.split(','))
-        });
-        for (const pedido of pedidoEQuantidades) {
-            if(!this.cardapio[pedido[0]]){
-                return 1
-            }
-            
-            if(pedido[1] == 0){
-                return 2
-            } 
-        return pedidoEQuantidades
-    }
-    }
-
     calcularValorDaCompra(metodoDePagamento, itens) {
         this.metodoDePagamento = metodoDePagamento;
         this.itens = itens;
-        if(!this.pagamento(metodoDePagamento)){
+
+        const formaDePagamento = new verificadorDePagamento().pagamentoValor(metodoDePagamento)
+        if (!formaDePagamento) {
             return ("Forma de pagamento inválida!")
         }
-        if(!itens.length){
+
+        const verificarPedidoCardapio = new produtoDosPedidos().pedidos(itens)
+        if (!verificarPedidoCardapio) {
             return ("Não há itens no carrinho de compra!")
         }
-        const pedidos = this.pedido(itens)
-        if(pedidos === 1){
+
+        if (verificarPedidoCardapio === 1) {
             return ("Item inválido!")
         }
-        if(pedidos === 2){
+        if (verificarPedidoCardapio === 2) {
             return ("Quantidade inválida!")
         }
-        if(pedidos === 3){
+        if (verificarPedidoCardapio === 3) {
             return ("Item extra não pode ser pedido sem o principal")
         }
+
         let valorTotalCompra = 0;
-        if(itens.length > 0){
-            for (const item of pedidos) {
-                if(this.cardapio[item[0]]){
-                    valorTotalCompra += (this.cardapio[item[0]] * parseFloat(item[1])) 
-                }
+        for (const item of verificarPedidoCardapio) {
+            if (cardapio[item[0]]) {
+                valorTotalCompra += (cardapio[item[0]] * parseFloat(item[1]))
             }
-            if(this.pagamento(metodoDePagamento) != 1){
-                valorTotalCompra += valorTotalCompra * this.pagamento(metodoDePagamento)
-            }
-            }
+        }
+        if (formaDePagamento != 1) {
+            valorTotalCompra += valorTotalCompra * formaDePagamento
+        }
+
         return "R$ " + valorTotalCompra.toFixed(2).replace('.', ',')
+        /* return "R$ " + Number(valorTotalCompra.toFixed(2)).toLocaleString('pt-BR', {
+            minimumFractionDigits: 2
+        }) */
     }
+}
+
+class produtoDosPedidos {
+    pedidos(itens) {
+        if (!itens.length) {
+            return false
+        }
+
+        let pedidoEQuantidades = []
+
+        itens.forEach(element => {
+            pedidoEQuantidades.push(element.split(','))
+        });
+
+        for (const pedido of pedidoEQuantidades) {
+            if (!cardapio[pedido[0]]) {
+                return 1
+            }
+
+            if (pedido[1] == 0) {
+                return 2
+            }
+        }
+
+        const chantily = (pedidoEQuantidades.some(item => item.includes('chantily')))
+        const cafe = (pedidoEQuantidades.some(item => item.includes('cafe')))
+        const queijo = (pedidoEQuantidades.some(item => item.includes('queijo')))
+        const sanduiche = (pedidoEQuantidades.some(item => item.includes('sanduiche')))
+
+        if (chantily && !cafe || queijo && !sanduiche) {
+            return 3
+        }
+
+        return pedidoEQuantidades
     }
+}
+
+class verificadorDePagamento {
+    pagamentoValor(metodoDePagamento) {
+        if (metodoDePagamento.includes('dinheiro')) {
+            return -0.05
+        } else if (metodoDePagamento.includes('credito')) {
+            return 0.03
+        } else if (metodoDePagamento.includes('debito')) {
+            return 1
+        } else {
+            return false
+        }
+    }
+}
+
 export { CaixaDaLanchonete };
